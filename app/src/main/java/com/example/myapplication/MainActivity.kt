b386e7a2cfb680ca5c7f329f29194fe77b3d42ed
TianZhao-007 <u7040076@anu.edu.au>
2022-09-15 22:36:50 +0800

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // get reference to all views
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val btn_login = findViewById<Button>(R.id.loginbt)
+        val btn_reset = findViewById<TextView>(R.id.forgetpass)
+
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            username.setText("comp8260")
+            password.setText("comp8260")
+        }
+
+        // set on-click listener
+        btn_login.setOnClickListener {
+            val user_name = username.text;
+            val password = password.text;
+            Toast.makeText(this@MainActivity, "Welcome,comp8260", Toast.LENGTH_LONG).show()
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+            val intent = Intent(this,NavActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+
+}
